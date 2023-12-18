c81fb804071129f7858e7e45101f9ba22a17fd78
erickseis <erickseis@gmail.com>
2023-12-17 22:40:20 -0300

    carga

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,28 @@ import Courses from '../components/courses';
 import Blog from '../components/blog';
 import About from '../components/about';
 import Review from '../components/review';
-// import Contact from '../components/form';
 import { Routes, Route } from 'react-router-dom';
 import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/registerForm';
 
+import { HashRouter } from 'react-router-dom';
+
 const App = () => {
   return (
     <div className='App'>
-      <Nav />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/courses' element={<Courses />} />
-        <Route path='/blog' element={<Blog />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/review' element={<Review />} />
-        <Route path='/login' element={<LoginForm />} />
-        <Route path='/registrarse' element={<RegisterForm />} />
-      </Routes>
-      <Footer />
+      <HashRouter>
+        <Nav />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/courses' element={<Courses />} />
+          <Route path='/blog' element={<Blog />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/review' element={<Review />} />
+          <Route path='/login' element={<LoginForm />} />
+          <Route path='/registrarse' element={<RegisterForm />} />
+        </Routes>
+        <Footer />
+      </HashRouter>
     </div>
   );
 };
